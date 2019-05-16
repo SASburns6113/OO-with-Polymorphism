9f25e946e5406207b27b31ad0429ac9258b1f0e8
JOSKILE <JOSKILE@WEIRDING12>
2019-05-16 11:49:48 -0700

    added parent class

---
+++ OO with Polymorphism/DMC_boss.cpp
@@ -0,0 +1,14 @@
+#include "DMC_boss.h"
+
+
+
+DMC_boss::DMC_boss()
+{
+
+
+}
+
+
+DMC_boss::~DMC_boss()
+{
+}
---
+++ OO with Polymorphism/DMC_boss.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class DMC_boss
+{
+public:
+	DMC_boss();
+	
+	void Speech(string theBattleText);
+
+	~DMC_boss();
+};
+
